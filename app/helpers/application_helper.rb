ed9642f8617b1f0f264d3990203963607f36059f
Horan, Daniel <dhoran@constantcontact.com>
2017-03-31 22:22:35 -0400

    first jersey

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def wordmark_image ( league, team, location = false )
+    "teams/#{league}/#{team}/wordmark_#{location}.svg"
+  end
+
+  def logo_image ( league, team )
+    "teams/#{league}/#{team}/logo.svg"
+  end
 end
